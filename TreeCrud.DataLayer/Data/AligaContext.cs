0ba443f98efa2be880a6748888f69c1b3b9cb21e
dani herrera <ctrl.alt.d@gmail.com>
2018-09-04 11:25:04 +0200

    data stored in a list, new step data on database

---
+++ TreeCrud.DataLayer/Data/AligaContext.cs
@@ -0,0 +1,13 @@
+﻿using TreeCrud.DataLayer.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace TreeCrud.DataLayer.Data
+{
+    public class AligaContext : DbContext
+    {
+        public AligaContext(DbContextOptions<AligaContext> options) : base(options) { }
+
+        public DbSet<Unitat> Unitats { get; set; }
+
+    }
+}
